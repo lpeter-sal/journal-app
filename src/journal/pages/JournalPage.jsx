6a2656c85a1b4a4b4e45f47792f4d5d4d61bcaae
Luis Pedro Salvador Hernandez <pedrosh09@gmail.com>
2024-01-03 15:14:17 -0700

    Se agrego la pantilla de registro, y tambien se agregaron los cascarones del home, se construyo el Navbar, SideBar y ToolsBar

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,12 +1,21 @@
 import { Typography } from "@mui/material"
+import { JornalLayout } from "../layout/JornalLayout"
 
 
 export const JournalPage = () => {
   return (
-    <>
-      <Typography variant="h1">JournalPage</Typography>
+    <JornalLayout>
+      
+
+      <Typography > Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate perferendis inventore laborum ratione ipsa cumque aperiam eaque, sed dicta consectetur distinctio libero vel ducimus repudiandae saepe commodi aut! Mollitia, non! </Typography>
+
+
+      {/* NothingSelected */}
+
+      {/* NoteView */}
+
+      
     
-    
-    </>
+    </JornalLayout>
   )
 }
