7365ba0e9c9861eccba60de9d31d06d78d5409e4
Luis Pedro Salvador Hernandez <pedrosh09@gmail.com>
2024-01-03 15:44:39 -0700

    Se agregaron el tema de las galerias de imagenes, y tambien boton flotante

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,3 +1,5 @@
+import { IconButton } from "@mui/material"
+import { AddOutlined } from "@mui/icons-material"
 import { JornalLayout } from "../layout/JornalLayout"
 import { NoteView, NothingSelectedView } from "../views"
 
@@ -7,10 +9,23 @@ export const JournalPage = () => {
     <JornalLayout>
       
       
-      {/* <NothingSelectedView /> */}
+      <NothingSelectedView />
 
-      <NoteView />
+      {/* <NoteView /> */}
 
+      <IconButton
+        size="large"
+        sx={{
+          color: "white",
+          backgroundColor: 'error.main',
+          ':hover': { backgroundColor: 'error.main', opacity: 0.9 },
+          position: 'fixed',
+          right: 50,
+          bottom: 50
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 30 }} />
+      </IconButton>
       
     
     </JornalLayout>
